2190c22bde3f6d1751bf07dcb9876dc25e3580fc
Shengqiu Li <lishengqiu.hit@gmail.com>
2016-10-01 01:29:19 +0800

    Fix example

--- examples/src/threadevent/main.go
+++ examples/src/threadevent/main.go
@@ -29,7 +29,7 @@ func NewMyFrame() *MyFrame {
     self := &MyFrame{}
     self.Frame = wx.NewFrame(wx.NullWindow, -1, "Thread Event Test")
     mainSizer := wx.NewBoxSizer(wx.HORIZONTAL)
-    g := wx.NewGauge(self, wx.ID_ANY, 100, wx.DefaultPosition, wx.NewSize(600, 40), 0)
+    g := wx.NewGauge(self, wx.ID_ANY, 100, wx.DefaultPosition, wx.NewSizeT(600, 40), 0)
     self.SetSizer(mainSizer)
     mainSizer.Add(g, 100, wx.ALL|wx.EXPAND, 50)
     self.Layout()
