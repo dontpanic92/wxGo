861acae747c60ae51def0dcf5ecbc9461c02dd95
Shengqiu Li <lishengqiu.hit@gmail.com>
2016-12-01 16:26:23 +0800

    Rename osx folder to darwin_amd64

--- wx/setup_windows_amd64.go
+++ wx/setup_windows_amd64.go
@@ -1,6 +1,7 @@
 package wx
 
 // #cgo CPPFLAGS: -I${SRCDIR}/windows_amd64/ -I${SRCDIR}/../wxWidgets/wxWidgets-3.1.0/include -D_FILE_OFFSET_BITS=64 -D__WXMSW__
-// #cgo LDFLAGS: -L${SRCDIR}/windows_amd64/lib -Wl,--subsystem,windows -mwindows -lwxmsw31u -lwxmsw31u_gl -lwxscintilla -lopengl32 -lglu32 -lwxregexu -lwxexpat -lwxtiff -lwxjpeg -lwxpng -lwxzlib -lrpcrt4 -loleaut32 -lole32 -luuid -lwinspool -lwinmm -lshell32 -lshlwapi -lcomctl32 -lcomdlg32 -ladvapi32 -lversion -lwsock32 -lgdi32 -lntdll -lmsvcrt
+// #cgo !wxgo_binary_package_build LDFLAGS: -L${SRCDIR}/windows_amd64/lib -lwxmsw31u -lwxmsw31u_gl -lwxscintilla -lwxregexu -lwxexpat -lwxtiff -lwxjpeg -lwxpng -lwxzlib 
+// #cgo LDFLAGS: -l:libstdc++.a -Wl,--subsystem,windows -mwindows -lopengl32 -lglu32 -lrpcrt4 -loleaut32 -lole32 -luuid -lwinspool -lwinmm -lshell32 -lshlwapi -lcomctl32 -lcomdlg32 -ladvapi32 -lversion -lwsock32 -lgdi32 -lntdll -lmsvcrt
 // #cgo CXXFLAGS: -fpermissive
 import "C"
