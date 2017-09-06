8a45f5aabc0862a6a1cedaae00e6eb77b0db4fa1
dontpanic <lishengqiu.hit@gmail.com>
2017-09-06 23:24:20 +0800

    Fix tag for mingw_workaround

--- wx/setup_windows_amd64.go
+++ wx/setup_windows_amd64.go
@@ -2,8 +2,8 @@ package wx
 
 // #cgo CPPFLAGS: -I${SRCDIR}/windows_amd64/ -I${SRCDIR}/../wxWidgets/wxWidgets-3.1.0/include -D_FILE_OFFSET_BITS=64 -D__WXMSW__
 // #cgo !wxgo_binary_package_build LDFLAGS: -L${SRCDIR}/windows_amd64/lib -lwxmsw31u -lwxmsw31u_gl -lwxscintilla -lwxregexu -lwxexpat -lwxtiff -lwxjpeg -lwxpng -lwxzlib
-// #cgo mingw-workaround LDFLAGS: -Wl,--subsystem,windows,--allow-multiple-definition
-// #cgo !mingw-workaround LDFLAGS: -Wl,--subsystem,windows
+// #cgo mingw_workaround LDFLAGS: -Wl,--subsystem,windows,--allow-multiple-definition
+// #cgo !mingw_workaround LDFLAGS: -Wl,--subsystem,windows
 // #cgo LDFLAGS: -l:libstdc++.a -mwindows -lopengl32 -lglu32 -lrpcrt4 -loleaut32 -lole32 -luuid -lwinspool -lwinmm -lshell32 -lshlwapi -lcomctl32 -lcomdlg32 -ladvapi32 -lversion -lwsock32 -lgdi32 -lntdll -lmsvcrt
 // #cgo CXXFLAGS: -fpermissive
 import "C"
