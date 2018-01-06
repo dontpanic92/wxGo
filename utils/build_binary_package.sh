662d00bbb7cfc83a03c5476333cd17f9d130c096
Li Shengqiu <lishengqiu.hit@gmail.com>
2018-01-06 21:48:36 +0800

    Update travis.yml

--- utils/build_binary_package.sh
+++ utils/build_binary_package.sh
@@ -2,7 +2,7 @@
 
 function go_version {
     version=$(go version)
-    regex="(go[0-9].[0-9].[0-9])"
+    regex="(go[0-9].[0-9](.[0-9])?)"
     if [[ $version =~ $regex ]]; then 
          echo ${BASH_REMATCH[1]}
     fi
